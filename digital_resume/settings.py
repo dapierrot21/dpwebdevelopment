c49fe35bd552fb74b432fc4b63693751598b2bcf
Dean Pierrot <dapierrot21@gmail.com>
2022-05-25 14:44:14 -0400

    Updated settings file.

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -89,7 +89,7 @@ SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
