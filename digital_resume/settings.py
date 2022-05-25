2e6e479f5990fc8da3e4a1e2e296688991a1ece4
Dean Pierrot <dapierrot21@gmail.com>
2022-05-25 14:18:42 -0400

    updated settings.

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dpwebdevelopment.herokuapp.com', 'dpwebdevelopment.co', 'dpwebdevelopment.com', 'dpwebdevelopment.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://dpwebdevelopment.herokuapp.com/']
 
 
 # Application definition
